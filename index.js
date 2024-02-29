f828265977bd30f2bf0f6fcb4277d065950c845c
Bogdan Agapie <55409109+AgapieBogdan@users.noreply.github.com>
2024-02-29 17:30:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var randomImage = ["./images/dice1.png","./images/dice2.png","./images/dice3.png","./images/dice4.png","./images/dice5.png","./images/dice6.png"];
 var randomNumber1 = Math.floor(Math.random()*randomImage.length);
-document.getElementById('image').src = randomImage[randomNumber1];
 var randomNumber2 = Math.floor(Math.random()*randomImage.length);
-document.getElementById('image2').src = randomImage[randomNumber2];
+
+var buttons = document.getElementsByClassName('roll');
+for (var i=0; i< buttons.length; i++)
+{   
+    
+    
+    buttons[i].addEventListener("click", function(){
+        document.getElementById('image').src = randomImage[randomNumber1];
+          
+
+        document.getElementById('image2').src = randomImage[randomNumber2];
+
 if(randomNumber1 > randomNumber2)
 {
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
@@ -13,3 +23,18 @@ else if(randomNumber1 < randomNumber2){
 else {
     document.querySelector("h1").innerHTML = "Draw!";
 }
+
+randomNumber1 = Math.floor(Math.random()*randomImage.length);
+randomNumber2 = Math.floor(Math.random()*randomImage.length);
+
+});
+
+}
+
+
+
+
+
+
+
+

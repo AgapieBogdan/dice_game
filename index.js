741fcfea1172aed510a1e2f78a265b87a903379c
Bogdan Agapie <55409109+AgapieBogdan@users.noreply.github.com>
2024-03-04 12:44:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var randomImage = ["./images/dice1.png","./images/dice2.png","./images/dice3.png","./images/dice4.png","./images/dice5.png","./images/dice6.png"];
 var randomNumber1 = Math.floor(Math.random()*randomImage.length);
 var randomNumber2 = Math.floor(Math.random()*randomImage.length);
+var player1score=0;
+var player2score = 0;
+var disp1 = document.getElementById("display1");
+var disp2 = document.getElementById("display2");
+
+
+//scor + imagine 
 
 var buttons = document.getElementsByClassName('roll');
 for (var i=0; i< buttons.length; i++)
@@ -16,9 +23,14 @@ for (var i=0; i< buttons.length; i++)
 if(randomNumber1 > randomNumber2)
 {
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    player1score++;
+disp1.innerHTML = player1score;    
 }
+
 else if(randomNumber1 < randomNumber2){
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    player2score++;
+    disp2.innerHTML = player2score;
 }
 else {
     document.querySelector("h1").innerHTML = "Draw!";
@@ -30,6 +42,16 @@ randomNumber2 = Math.floor(Math.random()*randomImage.length);
 });
 
 }
+
+//reset score 
+
+document.getElementById("resetare").addEventListener('click', function() {
+    disp1.innerHTML = "0";
+    disp2.innerHTML = "0";
+    player1score=0;
+    player2score =0;
+    document.querySelector("h1").innerHTML = "Hello, let's play a Dice Game!";
+});
 
 
 
